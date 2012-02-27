1e2b9dc21310578af4a5668291200a88a320a7af
Geoff Johnson <geoff.jay@gmail.com>
2012-02-27 14:39:19 -0800

    eliminated queue and spawn threads for select model

--- src/main.c
+++ src/main.c
@@ -146,9 +146,12 @@ signal_handler (int sig)
 void *
 client_manager (void *data)
 {
-    int ret;
+    int ret, nready;
     const int on = 1;
     struct sockaddr_in servaddr;
+    client *c = NULL;
+    struct client_data_t cd;
+    struct client_data_t *pcd;
 
     server *s = (server *)data;
 
@@ -167,19 +170,67 @@ client_manager (void *data)
     if (listen (s->fd, BACKLOG) < 0)
         CLDD_ERROR("Unable to listen on socket %ld", s->fd);
 
+/* --- replacing queue management with select --- */
+
     /* create the thread that monitors client requests */
-    ret = pthread_create (&s->client_queue_tid, NULL, client_queue_handler, s);
-    if (ret != 0)
-        CLDD_ERROR("Failed to create thread to manage client queue requests");
+//    ret = pthread_create (&s->client_queue_tid, NULL, client_queue_handler, s);
+//    if (ret != 0)
+//        CLDD_ERROR("Failed to create thread to manage client queue requests");
 
     /* create the thread that spawns client connections */
-    ret = pthread_create (&s->client_spawn_tid, NULL, client_spawn_handler, s);
-    if (ret != 0)
-        CLDD_ERROR("Failed to create thread that spawns client connections");
+//    ret = pthread_create (&s->client_spawn_tid, NULL, client_spawn_handler, s);
+//    if (ret != 0)
+//        CLDD_ERROR("Failed to create thread that spawns client connections");
 
     /* wait for the control threads to finish */
-    pthread_join (s->client_queue_tid, NULL);
-    pthread_join (s->client_spawn_tid, NULL);
+//    pthread_join (s->client_queue_tid, NULL);
+//    pthread_join (s->client_spawn_tid, NULL);
+
+/* --- old --- */
+
+    FD_ZERO(&s->rset);
+    s->maxfd = s->fd;
+
+    for (;;)
+    {
+        usleep (1000);
+
+        /* get the client data ready */
+        c = client_new ();
+        c->sa_len = sizeof (c->sa);
+
+        FD_SET(s->fd, &s->rset);
+        if ((nready = select (s->maxfd + 1, &s->rset, NULL, NULL, NULL)) < 0)
+        {
+            if (errno == EINTR)
+                continue;
+            else
+                CLDD_ERROR("select() error");
+        }
+
+        if (FD_ISSET(s->fd, &s->rset))
+        {
+            /* blocking call waiting for connections */
+            CLDD_MESSAGE("Waiting for a new client connection");
+            c->fd = accept (s->fd, (struct sockaddr *) &c->sa, &c->sa_len);
+            CLDD_MESSAGE("Received connection from (%s, %d)",
+                        inet_ntoa (c->sa.sin_addr), ntohs (c->sa.sin_port));
+
+            /* launch the thread for the client */
+            pthread_mutex_lock (&s->server_data_lock);
+            s->n_clients++;
+            s->n_max_connected = (s->n_clients > s->n_max_connected)
+                                ? s->n_clients : s->n_max_connected;
+            pthread_mutex_unlock (&s->server_data_lock);
+            cd.s = s;
+            cd.c = c;
+            pcd = &cd;
+            pthread_create (&c->tid, NULL, client_func, pcd);
+            CLDD_MESSAGE("Create client thread %ld", c->tid);
+        }
+
+        c = NULL;
+    }
 
     CLDD_MESSAGE("Exiting the client manager");
 
@@ -237,7 +288,7 @@ client_func (void *data)
 
     pthread_mutex_lock (&s->server_data_lock);
     s->n_clients--;
-    llist_remove (s->client_list, (void *)c, client_compare);
+//    llist_remove (s->client_list, (void *)c, client_compare);
     pthread_mutex_unlock (&s->server_data_lock);
     close (c->fd);
     client_free (c);
